5a35acc0751b6cc6ef1e03c5bdbda263da048f2f
Wisse <wissehes@gmail.com>
2023-11-27 21:14:30 +0100

    Initial Commit

---
+++ WLED App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WLED App
+//
+//  Created by Wisse Hes on 27/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WLED App/WLED_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WLED_AppApp.swift
+//  WLED App
+//
+//  Created by Wisse Hes on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WLED_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
