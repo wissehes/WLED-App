82a746fbe3e289568609372e83df4dad30dba5ba
Wisse <wissehes@gmail.com>
2023-11-27 22:28:38 +0100

    Add modelContainer for Device

--- WLED App/WLED_AppApp.swift
+++ WLED App/WLED_AppApp.swift
@@ -6,12 +6,16 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct WLED_AppApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: [
+                    Device.self
+                ])
         }
     }
 }
