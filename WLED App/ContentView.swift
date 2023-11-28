0b48b9c9077e4742806028b8126103034bf6cef8
Wisse <wissehes@gmail.com>
2023-11-28 22:28:41 +0100

    Update ContentView

--- WLED App/ContentView.swift
+++ WLED App/ContentView.swift
@@ -6,28 +6,48 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     
+    @Query private var devices: [Device]
+    
     @State private var addDevicesShowing = false
+    @State private var isOn = false
     
     var body: some View {
         NavigationSplitView {
             List {
-                Button("Add devices") {
-                    addDevicesShowing.toggle()
+                ForEach(devices) { device in
+                    DeviceItemView(device: device)
                 }
-            }.sheet(isPresented: $addDevicesShowing, content: {
-                DiscoverDevicesSheet()
-            })
+            }.navigationTitle("Devices")
+                .toolbar {
+                    ToolbarItem(placement: .primaryAction) {
+                        Button("Add devices", systemImage: "plus") {
+                            addDevicesShowing.toggle()
+                        }
+                    }
+                }
+                .task {
+                    for device in devices {
+                        await device.update()
+                    }
+                }
+                .sheet(isPresented: $addDevicesShowing, content: {
+                    DiscoverDevicesSheet()
+                })
         } detail: {
             // Detail view
             ContentUnavailableView("Select an item", systemImage: "network")
         }
-
+        
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: [
+            Device.self
+        ])
 }
