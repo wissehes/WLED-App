b4f949634b7e3a6324631feff3a369ede4bb54e9
Wisse <wissehes@gmail.com>
2023-11-28 21:19:08 +0100

    Update WLED api

--- WLED App/Services/WLED.swift
+++ WLED App/Services/WLED.swift
@@ -6,9 +6,17 @@
 //
 
 import Foundation
+import Alamofire
 
 class WLED {
     static let shared = WLED()
     
-    
+    func getInfo(address: String, port: String) async throws -> WLEDStateResponse {
+        let data = try await AF.request("http://\(address):\(port)/json")
+            .validate()
+            .serializingDecodable(WLEDStateResponse.self)
+            .value
+        
+        return data
+    }
 }
