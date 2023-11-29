64226e22579268c08f966b128d6a57470b3fa4f6
Wisse <wissehes@gmail.com>
2023-11-29 15:07:03 +0100

    Add comment

--- WLED App/Services/WLED.swift
+++ WLED App/Services/WLED.swift
@@ -11,6 +11,7 @@ import Alamofire
 class WLED {
     static let shared = WLED()
     
+    /// Get the JSON state/info object from a device
     func getInfo(address: String, port: String) async throws -> WLEDStateResponse {
         let data = try await AF.request("http://\(address):\(port)/json")
             .validate()
