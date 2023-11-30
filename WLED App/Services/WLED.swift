b85f80b5e104f829d25f13d79013671071597343
Wisse <wissehes@gmail.com>
2023-11-30 22:17:12 +0100

    Add getPresets method

--- WLED App/Services/WLED.swift
+++ WLED App/Services/WLED.swift
@@ -34,6 +34,25 @@ class WLED {
         return data
     }
     
+    func getPresets() async throws -> [WLEDPreset.Normalized] {
+        let data = try await AF.request(baseUrl.appending(path: "/presets.json"))
+            .validate()
+            .serializingDecodable(WLEDPresets.self)
+            .value
+        
+        var items: [WLEDPreset.Normalized] = [];
+        
+        for (id, preset) in data {
+            guard let normalized = WLEDPreset.Normalized(preset, id: id) else { continue }
+            
+            items.append(normalized)
+        }
+        
+        items.sort { $0.id < $1.id }
+        
+        return items
+    }
+    
     /// Set the on/off state of the device
     func setState(_ state: Bool) async {
         let body = [
