65f782e5be98145eeab4238a0550ac592a7f9b9a
Wisse <wissehes@gmail.com>
2023-12-03 22:16:56 +0100

    Connect to websockets in the webview

--- WLED App/Views/Device/DeviceWebView.swift
+++ WLED App/Views/Device/DeviceWebView.swift
@@ -6,17 +6,79 @@
 //
 
 import SwiftUI
+import Starscream
 
 struct DeviceWebView: View {
     var device: Device
+    
+    @State private var session: WebSocket?
     
     var body: some View {
         WebView(url: device.url)
             .navigationTitle(device.name)
             .navigationBarTitleDisplayMode(.inline)
+            .onAppear { self.initWS() }
+            .onDisappear { self.session?.disconnect() }
+    }
+    
+    // Connect via WebSockets so that the Device instance
+    // stays up-to-date with the web interface
+    private func initWS() {
+        let wsURL = URL(string: "ws://\(device.address):\(device.port)/ws")!
+        let request = URLRequest(url: wsURL)
+        
+        self.session = WebSocket(request: request)
+        
+        self.session?.onEvent = self.eventHandler(for:)
+        
+        self.session?.connect()
+    }
+    
+    private func eventHandler(for event: WebSocketEvent) -> Void {
+        switch event {
+        case .connected(_):
+            print("WS Connected")
+        case .text(let message):
+            print("Data: \(message)")
+            self.updateDevice(for: message)
+            
+        default:
+            break;
+        }
+    }
+    
+    private func updateDevice(for message: String) {
+        guard let data = message.data(using: .utf8) else { return }
+        do {
+            let decoded = try JSONDecoder().decode(WLEDStateResponse.self, from: data)
+            withAnimation {
+                self.device.update(response: decoded)
+            }
+        } catch {
+            print("Decoding failed")
+            print(error)
+        }
     }
 }
 
-//#Preview {
-//    DeviceWebView()
-//}
+#Preview {
+    let container = DataController.previewContainer
+    
+    let device = Device(
+        address: "192.168.178.113",
+        macAddress: nil,
+        port: nil,
+        name: nil,
+        presets: [],
+        isOnline: true,
+        isPoweredOn: true,
+        brightness: 255,
+        color: nil,
+        preset: nil
+    )
+    
+    return NavigationStack {
+        DeviceWebView(device: device)
+            .modelContainer(container)
+    }
+}
