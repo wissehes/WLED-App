c1b7f302b0bc34a730900eef8be572a09d50779a
Wisse <wissehes@gmail.com>
2023-12-04 19:56:02 +0100

    Add watchOS target

---
+++ WLED watchOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WLED watchOS Watch App
+//
+//  Created by Wisse Hes on 04/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WLED watchOS/WLED_watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WLED_watchOSApp.swift
+//  WLED watchOS Watch App
+//
+//  Created by Wisse Hes on 04/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WLED_watchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
