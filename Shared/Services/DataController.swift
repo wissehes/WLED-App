ced4152631318ae65b334dd7db10f8c50549ad7e
Wisse <wissehes@gmail.com>
2023-12-04 21:18:48 +0100

    Update DataController

--- Shared/Services/DataController.swift
+++ Shared/Services/DataController.swift
@@ -8,15 +8,26 @@
 import Foundation
 import SwiftData
 
+@MainActor
 class DataController {
     static let previewContainer: ModelContainer = {
         do {
             let config = ModelConfiguration(isStoredInMemoryOnly: true)
             let container = try ModelContainer(for: Device.self, configurations: config)
             
-//            let exampleDevice =
-            
-//            container.mainContext.insert(Device)
+            let device = Device(
+                address: "192.168.178.113",
+                macAddress: nil,
+                port: nil,
+                name: nil,
+                presets: [],
+                isOnline: true,
+                isPoweredOn: true,
+                brightness: 255,
+                color: nil,
+                preset: nil
+            )
+            container.mainContext.insert(device)
             
             return container
             
