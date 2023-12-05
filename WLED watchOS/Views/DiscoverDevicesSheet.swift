3273d0d6b9adabafbd9bd0c0569898ea5c155cd3
Wisse <wissehes@gmail.com>
2023-12-05 15:35:45 +0100

    Create DiscoverDevicesSheet

---
+++ WLED watchOS/Views/DiscoverDevicesSheet.swift
@@ -0,0 +1,71 @@
+//
+//  DiscoverDevicesSheet.swift
+//  WLED Watch App
+//
+//  Created by Wisse Hes on 05/12/2023.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DiscoverDevicesSheet: View {
+    @Environment(\.dismiss) private var dismiss
+        
+    @State private var discovery = DiscoveryService()
+    
+    var body: some View {
+        NavigationStack {
+            List(discovery.devices, rowContent: AddDeviceItemView.init)
+                .navigationTitle("Discover")
+                .navigationBarTitleDisplayMode(.large)
+                .overlay {
+                    if discovery.devices.isEmpty {
+                        ProgressView()
+                    }
+                }
+        }
+    }
+    
+    struct AddDeviceItemView: View {
+        var device: Device
+        
+        init(_ device: Device) {
+            self.device = device
+        }
+        
+        @Query private var devices: [Device]
+        @Environment(\.modelContext) private var modelContext
+
+        var isAdded: Bool {
+            devices.contains { $0.macAddress == device.macAddress }
+        }
+        
+        var buttonIcon: String {
+            isAdded ? "checkmark.circle" :"plus"
+        }
+        
+        var body: some View {
+            HStack {
+                Text(device.name)
+                
+                Spacer()
+                
+                Button("Add", systemImage: buttonIcon) {
+                    addDevice(self.device)
+                }
+                    .labelStyle(.iconOnly)
+                    .foregroundStyle(.accent)
+                    .disabled(isAdded)
+            }
+        }
+        
+        private func addDevice(_ device: Device) {
+            modelContext.insert(device)
+        }
+    }
+}
+
+#Preview {
+    DiscoverDevicesSheet()
+        .modelContainer(DataController.previewContainer)
+}
