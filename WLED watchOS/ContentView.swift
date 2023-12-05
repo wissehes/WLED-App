6e9b046cb17a1d5122b45c50599db6ed00144ad8
Wisse <wissehes@gmail.com>
2023-12-05 15:36:13 +0100

    Add sheet and toolbar button

--- WLED watchOS/ContentView.swift
+++ WLED watchOS/ContentView.swift
@@ -10,9 +10,10 @@ import SwiftData
 
 struct ContentView: View {
     
-    @Query var devices: [Device]
+    @Query private var devices: [Device]
     
-    @State var selectedDevice: Device?
+    @State private var selectedDevice: Device?
+    @State private var discoverSheetShowing = false
     
     var body: some View {
         NavigationSplitView {
@@ -24,9 +25,19 @@ struct ContentView: View {
                         selectedDevice = device
                     }
                 }
-            }.task {
+            }
+            .task {
                 await updateDevices()
-            }.overlay {
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Discover devices", systemImage: "plus") {
+                        discoverSheetShowing.toggle()
+                    }
+                }
+            }
+            .sheet(isPresented: $discoverSheetShowing, content: DiscoverDevicesSheet.init)
+            .overlay {
                 if devices.isEmpty {
                     ContentUnavailableView("No devices", systemImage: "lightbulb.min.badge.exclamationmark")
                 }
